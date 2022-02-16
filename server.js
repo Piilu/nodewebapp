055f3ab1d6248b3be26423b54a1db80c412e0ec4
Piilu <piilrainer@gmail.com>
2022-02-16 02:15:14 +0200

    backend

--- server.js
+++ server.js
@@ -33,25 +33,38 @@ app.use(sessions({
 
 var connection;
 //CONNECT TO DATABASE//
-function connectDb() {
+var db_config = {
+  connectionLimit: 10,
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_DATABASE,
+};
 
-  connection = mysql.createPool({
-    connectionLimit: 10,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
   });
 }
-connectDb();
 
-
-connection.on('error', function (err) {
-  // in case of error
-  console.log(err.code);
-  console.log(err.fatal);
-});
-
+handleDisconnect();
 ////////////
 
 
